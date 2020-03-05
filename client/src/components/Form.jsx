abf7dea2db96ce2029b6565184fdd80c27a3c41f
frankie siino <frankie.siino@gmail.com>
2020-03-05 00:17:07 -0800

    Convert to react hooks

--- client/src/components/Form.jsx
+++ client/src/components/Form.jsx
@@ -1,77 +1,55 @@
-import React, { Component, useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import '../style/form.css';
 import axios from 'axios';
 import Results from './Results';
-import { cloneDeep } from 'lodash'
 
-class Form extends Component {
-  constructor() {
-    super();
-    this.state = {
-      fieldsets: [{
-        nutrient: '',
-        min: '',
-        max: ''
-      }],
-      results: [],
-      notFound: false,
-      isLoading: false,
-      errors: []
-    }
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.handleClearResults = this.handleClearResults.bind(this);
+const Form = () => {
+  const newFieldset = {
+    nutrient: '',
+    min: '',
+    max: ''
   }
+  const [fieldsets, setFieldsets] = useState([newFieldset])
+  const [results, setResults] = useState([])
+  const [notFound, setNotFound] = useState(false)
+  const [isLoading, setLoading] = useState(false)
+  const [errors, setErrors] = useState([])
 
-  handleClearResults(e) {
+  const handleClearResults = e => {
     e.preventDefault();
-    this.setState({ 
-      results: [],
-      notFound: false
-    })
+    setResults([])
+    setNotFound(false)
   }
 
-  clearAllFields() {
-    let fieldsets = this.state.fieldsets.slice(0,1);
-    for (let key in fieldsets[0]) fieldsets[0][key] = '';
-    this.setState({
-      fieldsets: [...fieldsets],
-      errors: []
-    })
+  const clearAllFields = () => {
+    let fieldsetsCopy = fieldsets.slice(0,1);
+    for (let key in fieldsetsCopy[0]) fieldsetsCopy[0][key] = '';
+    setFieldsets([...fieldsetsCopy])
+    setErrors([])
   }
 
-  addNewFieldset() {
-    let fieldsets = this.state.fieldsets.slice();
-    fieldsets.push({
-      nutrient: '',
-      min: '',
-      max: ''
-    })
-    this.setState({
-      fieldsets: fieldsets
-    })
+  const addNewFieldset = () => {
+    let fieldsetsCopy = fieldsets.slice();
+    fieldsetsCopy.push(newFieldset)
+    setFieldsets(fieldsetsCopy)
   }
 
-  removeFieldset(e,fieldsetNum) { //TODO: Not removing properly
-    let fieldsetsCopy = this.state.fieldsets.slice();
+  const removeFieldset = (e, fieldsetNum) => { //TODO: Not removing properly
+    let fieldsetsCopy = fieldsets.slice();
     fieldsetsCopy.splice(fieldsetNum, 1); 
-    this.setState({
-      fieldsets: fieldsetsCopy
-    })
+    setFieldsets(fieldsetsCopy)
   }
 
-  handleSubmit(e) {
+  const handleSubmit = (e) => {
     e.preventDefault();
-    this.setState({
-      isLoading: true
-    })
+    setLoading(true)
     e.preventDefault();
-    const fieldsets = this.state.fieldsets;
-    for (let i = 0; i < fieldsets.length; i++) {
-      const fieldset = fieldsets[i];
-      let nutrient = fieldset.nutrient;
-      let min = fieldset.min;
-      let max = fieldset.max;
+    const fieldsetsCopy = fieldsets;
+    for (let i = 0; i < fieldsetsCopy.length; i++) {
+      const fieldsetCopy = fieldsetsCopy[i];
+      let nutrient = fieldsetCopy.nutrient;
+      let min = fieldsetCopy.min;
+      let max = fieldsetCopy.max;
       if (!nutrient) continue; 
       if (!min) min = 0;
       if (!max) max = 1000;
@@ -79,88 +57,81 @@ class Form extends Component {
       axios.get(`/api/foods/${nutrient}/min=${min}&max=${max}`)
         .then(res => {
           if (res.data.length) {
-            this.setState({
-              results: res.data,
-              isLoading: false
-            })
+            setResults(res.data)
+            setLoading(false)
           } else {
-          this.setState({
-            results: [],
-            isLoading: false,
-            notFound: true
-          })
+            setResults([])
+            setLoading(false)
+            setNotFound(true)
           }
         })
-        .catch(err => this.setState({ errors: err }));
+        .catch(err => setErrors(...errors))
      }  
   }
 
-  handleChange(field, fieldsetNum) {
+  const handleChange = (field, fieldsetNum) => {
     return e => {
-      let fieldsets = this.state.fieldsets.slice();
-      fieldsets[fieldsetNum][field] = e.target.value;
-      this.setState({
-        fieldsets: fieldsets
-      })
+      let fieldsetsCopy = fieldsets.slice();
+      fieldsetsCopy[fieldsetNum][field] = e.target.value;
+      setFieldsets(fieldsetsCopy)
     }
   }
 
-  render() {
-    const loadedFoods = (
-      this.state.results ? 
-        this.state.results.map((result, i) => 
-        <li key={i}>{result.name} 
-          <ul>{result.nutrients.map((nutrient, j) => 
-            <li key={j}>{nutrient.nutrient} - {nutrient.value}{nutrient.unit}</li>)}
-          </ul>
-        </li>) 
-      : null
-    )
+  const loadedFoods = (
+    results ? 
+      results.map((result, i) => 
+      <li key={i}>{result.name} 
+        <ul>{result.nutrients.map((nutrient, j) => 
+          <li key={j}>{nutrient.nutrient} - {nutrient.value}{nutrient.unit}</li>)}
+        </ul>
+      </li>) 
+    : null
+  )
 
-    return (
-      <>
-        <button onClick={() => this.addNewFieldset()}>Add Another Filter</button>
-        <button onClick={() => this.clearAllFields()}>Clear All Fieldsets</button>
-        <form onSubmit={this.handleSubmit}>
-          <div className="fieldsets-wrapper"> 
-            {this.state.fieldsets.map((fieldset, i) => {
-              return (
-                <div className="fieldset-wrapper" 
-                  key={i}
-                >
-                  <label>Nutrient:
-                    <input required type="text"
-                      value={this.state.fieldsets[i].nutrient}
-                      onChange={this.handleChange("nutrient", i)} 
-                    />
-                  </label>
-                  <label>Minimum:
-                    <input type="text"
-                      value={this.state.fieldsets[i].min}
-                      onChange={this.handleChange("min", i)}
-                    />
-                  </label>
-                  <label>Maximum:
-                    <input type="text"
-                      value={this.state.fieldsets[i].max}
-                      onChange={this.handleChange("max", i)}
-                    />
-                  </label>
-                  {i > 0 ? <input type="submit" onClick={(e, i) => this.removeFieldset(e,i)} value="Remove Fieldset" /> : null}
-                </div>
-              )
-            })}
-          </div>
-          <input type="submit" value="Submit Query" /> 
-          <button onClick={this.handleClearResults}>Clear Results</button>
-          {/* TODO: Return key doesnt submit, but rather hits New filter btn */}
-        </form>
-        <div>
-          <Results loadedFoods={loadedFoods} isLoading={this.state.isLoading} notFound={this.state.notFound} errors={this.errors} />
+  return (
+    <>
+      <button onClick={() => addNewFieldset()}>Add Another Filter</button>
+      <button onClick={() => clearAllFields()}>Clear All Fieldsets</button>
+      <form onSubmit={handleSubmit}>
+        <div className="fieldsets-wrapper"> 
+          {fieldsets.map((fieldset, i) => {
+            return (
+              <div className="fieldset-wrapper" 
+                key={i}
+              >
+                <label>Nutrient:
+                  <input required type="text"
+                    value={fieldsets[i].nutrient}
+                    onChange={handleChange("nutrient", i)} 
+                  />
+                </label>
+                <label>Minimum:
+                  <input type="text"
+                    value={fieldsets[i].min}
+                    onChange={handleChange("min", i)}
+                  />
+                </label>
+                <label>Maximum:
+                  <input type="text"
+                    value={fieldsets[i].max}
+                    onChange={handleChange("max", i)}
+                  />
+                </label>
+                {i > 0 ? <input type="submit" onClick={(e, i) => removeFieldset(e,i)} value="Remove Fieldset" /> : null}
+              </div>
+            )
+          })}
         </div>
-      </>
-    )
-  }
+        <input type="submit" value="Submit Query" /> 
+        <button onClick={handleClearResults}>Clear Results</button>
+        {/* TODO: Return key doesnt submit, but rather hits New filter btn */}
+      </form>
+      <div>
+        <Results loadedFoods={loadedFoods} isLoading={isLoading} notFound={notFound} errors={errors} />
+      </div>
+    </>
+  )
+
 }
 
 export default Form;
