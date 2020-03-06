4ae5f2695fcded5e46313119ae567c75b5f2e25e
frankie siino <frankie.siino@gmail.com>
2020-03-06 00:40:18 -0800

    Refactor routes

--- routes/api/foods.js
+++ routes/api/foods.js
@@ -17,23 +17,20 @@ router.get('/:ndbno', (req, res) => {
     .catch(err => res.status(404).json({ nofoodfound: 'No food found with that id' }))
 });
 
-//TODO: chain queries for multiple search query params
-// http://localhost:5000/api/foods/nutrient=ash&min=0&max=10/nutrient=protein&min=5&max=20
+//TODO: Test: http://localhost:5000/api/foods/nutrient=ash&min=0&max=1/nutrient=protein&min=0&max=0.01/nutrient=alcohol&min=0&max=40&nutrient=carb&min=0&max=0.03
 router.get('/*', (req, res) => {
-  let queries = [];
-  let fieldsets = req.params[0].split('/')
-  let parsedSets = fieldsets.map(fieldset => fieldset.split('&'))
-  let newFieldsets = [];
-  for (let i = 0; i < parsedSets.length; i++) {
-    let obj = {};
-    for (let j = 0; j < parsedSets[i].length; j++) {
-      const pair = parsedSets[i][j].split('=')
-      obj[pair[0]] = pair[1]
+  let promise = new Promise(res => {
+    const fieldsets = req.params[0].split('/')
+    const parsedSets = fieldsets.map(fieldset => fieldset.split('&'))
+    let newFieldsets = [];
+    for (let i = 0; i < parsedSets.length; i++) {
+      let obj = {};
+      for (let j = 0; j < parsedSets[i].length; j++) {
+        const pair = parsedSets[i][j].split('=')
+        obj[pair[0]] = pair[1]
+      }
+      newFieldsets.push(obj)
     }
-    newFieldsets.push(obj)
-  }
-
-  let promise = new Promise((res, rej) => {
     let queries = newFieldsets.map((newFieldset, idx) => {
       let query = {
         nutrients: {
@@ -41,21 +38,19 @@ router.get('/*', (req, res) => {
             nutrient: "",
             value: {
               $gt: 0,
-              $lte: 20
+              $lte: 100
             }
           }
         }
       };
-      query.nutrients.$elemMatch.nutrient = newFieldset.nutrient
+      query.nutrients.$elemMatch.nutrient = new RegExp(["", newFieldset.nutrient, ""].join(""), "i");
       query.nutrients.$elemMatch.value.$gt = newFieldset.min;
       query.nutrients.$elemMatch.value.$lte = newFieldset.max;
       return query;
+    })
+    res(queries)
   })
-  res(queries)
-})
   promise.then(queries => {
-    console.log(queries)
-    console.log(JSON.stringify(queries,null,2))
     Food.find({
       $and: queries
     })
@@ -63,8 +58,8 @@ router.get('/*', (req, res) => {
       .then(foods => res.json(foods))
       .catch(err => res.status(404).json({ nofoodsfound: 'No foods found with the specified ingredient parameter(s)' }));
   })
-  .catch(err => console.log(err))
-  
+    .catch(err => console.log(err))
+
 });
 
 module.exports = router;
