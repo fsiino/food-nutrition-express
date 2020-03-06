8883cfdaf36582f6f629e31b76d596d7a5c72414
frankie siino <frankie.siino@gmail.com>
2020-03-05 16:08:39 -0800

    Wrap query in promise

--- routes/api/foods.js
+++ routes/api/foods.js
@@ -18,34 +18,53 @@ router.get('/:ndbno', (req, res) => {
 });
 
 //TODO: chain queries for multiple search query params
-// router.get('/:nutrient/min=:min&max=:max*', (req, res) => {
+// http://localhost:5000/api/foods/nutrient=ash&min=0&max=10/nutrient=protein&min=5&max=20
 router.get('/*', (req, res) => {
+  let queries = [];
   let fieldsets = req.params[0].split('/')
   let parsedSets = fieldsets.map(fieldset => fieldset.split('&'))
-  let arr = [];
+  let newFieldsets = [];
   for (let i = 0; i < parsedSets.length; i++) {
     let obj = {};
     for (let j = 0; j < parsedSets[i].length; j++) {
       const pair = parsedSets[i][j].split('=')
       obj[pair[0]] = pair[1]
     }
-    arr.push(obj)
+    newFieldsets.push(obj)
   }
-  console.log(arr)
 
-  let nutrient = req.params.nutrient
-  // Creates a regex of: /SomeStringToFind/i 
-  let nutrientRegex = new RegExp(["", nutrient, ""].join(""), "i");
-  let min = req.params.min 
-  let max = req.params.max 
-  Food.find({
-    $and: [
-      { nutrients: { $elemMatch: { nutrient: nutrientRegex, value: { $gt: min, $lte: max } } } }
-    ]
+  let promise = new Promise((res, rej) => {
+    let queries = newFieldsets.map((newFieldset, idx) => {
+      let query = {
+        nutrients: {
+          $elemMatch: {
+            nutrient: "",
+            value: {
+              $gt: 0,
+              $lte: 20
+            }
+          }
+        }
+      };
+      query.nutrients.$elemMatch.nutrient = newFieldset.nutrient
+      query.nutrients.$elemMatch.value.$gt = newFieldset.min;
+      query.nutrients.$elemMatch.value.$lte = newFieldset.max;
+      return query;
   })
-    .sort({ name: 1 })
-    .then(foods => res.json(foods))
-    .catch(err => res.status(404).json({ nofoodsfound: 'No foods found with the specified ingredient parameter(s)' }));
+  res(queries)
+})
+  promise.then(queries => {
+    console.log(queries)
+    console.log(JSON.stringify(queries,null,2))
+    Food.find({
+      $and: queries
+    })
+      .sort({ name: 1 })
+      .then(foods => res.json(foods))
+      .catch(err => res.status(404).json({ nofoodsfound: 'No foods found with the specified ingredient parameter(s)' }));
+  })
+  .catch(err => console.log(err))
+  
 });
 
 module.exports = router;
