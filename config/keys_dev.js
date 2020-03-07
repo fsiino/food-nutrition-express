7b9f2a52589157b12fd674f91d16168b609d9f7f
frankie siino <frankie.siino@gmail.com>
2020-03-06 16:44:39 -0800

    Remove proxy for heroku deployment

--- config/keys_dev.js
+++ config/keys_dev.js
@@ -1,3 +1,3 @@
 module.exports = {
-  mongoURI: "mongodb+srv://admin:UBqomHaGss3aPnW2@cluster0-vjsnf.mongodb.net/test?retryWrites=true&w=majority"
+  mongoURI: "mongodb+srv://admin:USzaEpxazCRuLX1J@cluster0-vjsnf.mongodb.net/test?retryWrites=true&w=majority"
 };
