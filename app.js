70420a37b1b4cdc0da3ab684a0ebdfaecc917a4a
frankie siino <frankie.siino@gmail.com>
2020-03-06 16:52:46 -0800

    Heroku deployment

--- app.js
+++ app.js
@@ -15,6 +15,13 @@ mongoose
   .then(() => console.log("Connected to MongoDB successfully"))
   .catch(err => console.log(err));
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'))
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', index.html));
+  })
+}
+
 app.get('/', (req, res) => res.send("Hello World"));
 
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -22,12 +29,6 @@ app.use(bodyParser.json());
 
 app.use('/api/foods', foods);
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'))
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', index.html));
-  })
-}
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server running on port ${port}`));
